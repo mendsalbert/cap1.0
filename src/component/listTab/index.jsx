ef489ee11fbfb10bd3982cc8920df4f1606ec569
mendsalbert <mendsalbert@gmail.com>
2023-11-25 12:46:27 +0000

    cap

--- src/component/listTab/index.jsx
+++ src/component/listTab/index.jsx
@@ -10,7 +10,7 @@ function ListTab({ pageSize }) {
     <div className="w-full rounded-lg bg-white px-[24px] py-[20px] dark:bg-darkblack-600">
       <div className="flex flex-col space-y-5">
         <div className="flex h-[56px] w-full space-x-4">
-          <Search />
+          {/* <Search /> */}
           {/* <Filter options={["January", "February", "March"]} /> */}
         </div>
         {/* <FilterFull /> */}
