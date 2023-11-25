57c08690e63d2eb161aee5602d90b80b571fab23
mendsalbert <mendsalbert@gmail.com>
2023-11-25 12:53:26 +0000

    cap

--- src/app/(sharedLayout)/task/page.jsx
+++ src/app/(sharedLayout)/task/page.jsx
@@ -75,7 +75,7 @@ function Task() {
         </div>
       </div>
       <section className="2xl:w-70 w-full 2xl:mb-0 mb-6">
-        <h1 className="text-lg">faseffd</h1>
+        <h1 className="text-lg py-2">faseffd</h1>
         <ListTab pageSize={9} />
       </section>
       {/* <section className="2xl:flex-1 w-full">
