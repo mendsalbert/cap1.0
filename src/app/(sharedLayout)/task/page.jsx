f01d0333b31c624af82707c4a976a459e8b408c1
mendsalbert <mendsalbert@gmail.com>
2023-11-25 13:02:35 +0000

    cap

--- src/app/(sharedLayout)/task/page.jsx
+++ src/app/(sharedLayout)/task/page.jsx
@@ -76,7 +76,7 @@ function Task() {
       </div>
       <section className="2xl:w-70 w-full 2xl:mb-0 mb-6">
         {/* <h1 className="text-lg py-2 font-bold">(faseffd)</h1> */}
-        <button className="btn mb-3">
+        <button className="btn mb-3 mt-2">
           Inbox
           <div className="badge">+99</div>
         </button>
