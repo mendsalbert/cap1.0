b2a6f0682e6d2916d512026a021f3842312f6a2e
mendsalbert <mendsalbert@gmail.com>
2023-11-26 14:58:49 +0000

    cap

--- src/app/(sharedLayout)/footprint/page.jsx
+++ src/app/(sharedLayout)/footprint/page.jsx
@@ -5,6 +5,13 @@ function FootPrintComponent() {
   return (
     <>
       {/* TODO: this must be showend to the admin alone */}
+      <button
+        onClick={() => document.getElementById("my_modal_4").showModal()}
+        className="bg-darkblack-600 text-white py-1.5  flex flex-row items-center space-x-2 px-2 rounded-full mb-2"
+      >
+        <IconCirclePlus />
+        <span>Add Campaign</span>
+      </button>
       <div className="grid lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 lg:gap-4 xl:gap-6">
         {footprints?.map((footprint) => (
           <FootPrint key={footprint.id} footprint={footprint} />
