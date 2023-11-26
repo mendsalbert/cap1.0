a10efa1ba39a8568f0dc17b6c2ec4fe1f4d5884e
mendsalbert <mendsalbert@gmail.com>
2023-11-26 11:58:35 +0000

    cap

--- src/app/(sharedLayout)/donation/page.jsx
+++ src/app/(sharedLayout)/donation/page.jsx
@@ -30,7 +30,7 @@ function MyWallet() {
             Fill in the details to add a donation
           </h3>
           {/* name, category */}
-          <div className="space-x-4">
+          <div className="space-x-4 py-2">
             <input
               type="text"
               placeholder="Type here"
