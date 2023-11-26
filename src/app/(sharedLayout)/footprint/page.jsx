43be947143478558d9923f55b4222adefd7210d0
mendsalbert <mendsalbert@gmail.com>
2023-11-26 14:56:42 +0000

    cap

--- src/app/(sharedLayout)/footprint/page.jsx
+++ src/app/(sharedLayout)/footprint/page.jsx
@@ -4,6 +4,7 @@ import footprints from "@/data/footprint";
 function FootPrintComponent() {
   return (
     <>
+      {/* TODO: this must be showend to the admin alone */}
       <div className="grid lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 lg:gap-4 xl:gap-6">
         {footprints?.map((footprint) => (
           <FootPrint key={footprint.id} footprint={footprint} />
