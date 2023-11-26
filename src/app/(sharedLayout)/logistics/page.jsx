68e456042b6b2ed95568bc69b245fa0d163f558f
mendsalbert <mendsalbert@gmail.com>
2023-11-26 15:01:03 +0000

    cap

--- src/app/(sharedLayout)/logistics/page.jsx
+++ src/app/(sharedLayout)/logistics/page.jsx
@@ -6,6 +6,13 @@ import { IconCirclePlus, IconX } from "@tabler/icons-react";
 function LogisticsComponent() {
   return (
     <>
+      <button
+        onClick={() => document.getElementById("my_modal_4").showModal()}
+        className="bg-darkblack-600 text-white py-1.5  flex flex-row items-center space-x-2 px-2 rounded-full mb-2"
+      >
+        <IconCirclePlus />
+        <span>Add Product</span>
+      </button>
       <div className="grid lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 lg:gap-4 xl:gap-6">
         {logistics?.map((logistic) => (
           <Logistics key={logistic.id} logistic={logistic} />
