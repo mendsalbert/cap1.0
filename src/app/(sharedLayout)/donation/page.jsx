d92a9ab3d69a83217d8740d485d461d4160399bd
mendsalbert <mendsalbert@gmail.com>
2023-11-26 12:44:01 +0000

    cap

--- src/app/(sharedLayout)/donation/page.jsx
+++ src/app/(sharedLayout)/donation/page.jsx
@@ -73,7 +73,7 @@ function MyWallet() {
             placeholder="Bio"
             className="textarea textarea-bordered textarea-lg mt-2 w-full "
           ></textarea>
-          <button className="bg-green-500 hover:bg-green-600 w-full my-1">
+          <button className="bg-green-500 hover:bg-green-600 w-full my-1 p-6">
             Upload
           </button>
           {/* add */}
