80e8bf738ac0d73d5734d702e84cebc4456595b4
mendsalbert <mendsalbert@gmail.com>
2023-11-26 10:01:46 +0000

    cap

--- src/component/integration/Integration.jsx
+++ src/component/integration/Integration.jsx
@@ -122,6 +122,7 @@ function Integration({ integration }) {
             <form method="dialog">
               {/* if there is a button in form, it will close the modal
               <button className="btn  bg-error-200">Close</button> */}
+              x
             </form>
           </div>
         </div>
