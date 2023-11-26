afd2fb33b079d27083a0afb298dffb432f81281e
mendsalbert <mendsalbert@gmail.com>
2023-11-26 09:54:37 +0000

    cap

--- src/component/integration/Integration.jsx
+++ src/component/integration/Integration.jsx
@@ -47,10 +47,6 @@ function Integration({ integration }) {
 
       <dialog id="my_modal_1" className="modal ">
         <div className="modal-box w-11/12 max-w-5xl dark:bg-[#1d1e23]">
-          {/* <h3 className="font-bold text-lg">Hello!</h3> */}
-          {/* <p className="py-4">
-            Press ESC key or click the button below to close
-          </p> */}
           <div className="flex  space-x-3">
             <img src="/war2.png" width={500} className="rounded-lg" />
             <div className="flex flex-col">
