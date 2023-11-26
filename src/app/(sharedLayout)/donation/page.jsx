979509060e611b7bd2a11d3f410a47cb7518c8ea
mendsalbert <mendsalbert@gmail.com>
2023-11-26 12:36:18 +0000

    cap

--- src/app/(sharedLayout)/donation/page.jsx
+++ src/app/(sharedLayout)/donation/page.jsx
@@ -62,6 +62,20 @@ function MyWallet() {
             </select>
           </div>
           {/* file input */}
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Pick a file</span>
+              <span className="label-text-alt">Alt label</span>
+            </label>
+            <input
+              type="file"
+              className="file-input file-input-bordered w-full max-w-xs"
+            />
+            <label className="label">
+              <span className="label-text-alt">Alt label</span>
+              <span className="label-text-alt">Alt label</span>
+            </label>
+          </div>
           {/* description */}
           <textarea
             placeholder="Bio"
