ee1b3191ac75c7cfcc6555b0ab7b6a974d7bd147
mendsalbert <mendsalbert@gmail.com>
2023-11-26 15:00:50 +0000

    cap

--- src/app/(sharedLayout)/logistics/page.jsx
+++ src/app/(sharedLayout)/logistics/page.jsx
@@ -1,14 +1,11 @@
+"use client";
 import Logistics from "@/component/logistics/Logistics";
 import logistics from "@/data/logistics";
+import { IconCirclePlus, IconX } from "@tabler/icons-react";
 
 function LogisticsComponent() {
   return (
     <>
-      {/* <section className="2xl:w-[424px]">
-        <AddBalance />
-        <Wallet/>
-      </section> */}
-
       <div className="grid lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 lg:gap-4 xl:gap-6">
         {logistics?.map((logistic) => (
           <Logistics key={logistic.id} logistic={logistic} />
