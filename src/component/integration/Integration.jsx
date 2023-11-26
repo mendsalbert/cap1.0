bef9439e226bd276efea3799e66690f159ad1b42
mendsalbert <mendsalbert@gmail.com>
2023-11-26 10:02:09 +0000

    cap

--- src/component/integration/Integration.jsx
+++ src/component/integration/Integration.jsx
@@ -120,9 +120,8 @@ function Integration({ integration }) {
           </div>
           <div className="modal-action absolute -top-5 right-4">
             <form method="dialog">
-              {/* if there is a button in form, it will close the modal
-              <button className="btn  bg-error-200">Close</button> */}
-              x
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn  bg-error-200">Close</button>
             </form>
           </div>
         </div>
