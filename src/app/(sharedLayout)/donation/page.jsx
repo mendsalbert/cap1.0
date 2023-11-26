451098760ae3439b0c55709ac6c19cab07597e93
mendsalbert <mendsalbert@gmail.com>
2023-11-26 11:12:33 +0000

    cap

--- src/app/(sharedLayout)/donation/page.jsx
+++ src/app/(sharedLayout)/donation/page.jsx
@@ -25,7 +25,7 @@ function MyWallet() {
       </div>
 
       <dialog id="my_modal_4" className="modal">
-        <div className="modal-box w-11/12 max-w-5xl">
+        <div className="modal-box w-11/12 max-w-5xl dark:bg-[#1d1e23]">
           <h3 className="font-bold text-lg">Hello!</h3>
           <p className="py-4">Click the button below to close</p>
           <div className="modal-action">
