425c1929669c8acee491d00357efe526be017d6d
mendsalbert <mendsalbert@gmail.com>
2023-11-26 11:02:47 +0000

    cap

--- src/app/(sharedLayout)/donation/page.jsx
+++ src/app/(sharedLayout)/donation/page.jsx
@@ -12,7 +12,7 @@ function MyWallet() {
         <AddBalance />
         <Wallet />
       </section> */}
-      <button>sefsdf</button>
+      <button>Add Donation</button>
 
       <div className="grid lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 lg:gap-4 xl:gap-6">
         {integrations?.map((integration) => (
