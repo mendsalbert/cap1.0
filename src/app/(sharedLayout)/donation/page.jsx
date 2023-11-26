234e6aedd27b7f40ebca3ab152f9ab0d257e6acb
mendsalbert <mendsalbert@gmail.com>
2023-11-26 11:25:31 +0000

    cap

--- src/app/(sharedLayout)/donation/page.jsx
+++ src/app/(sharedLayout)/donation/page.jsx
@@ -26,7 +26,9 @@ function MyWallet() {
 
       <dialog id="my_modal_4" className="modal">
         <div className="modal-box w-6/12 max-w-5xl dark:bg-[#1d1e23] dark:text-white">
-          <h3 className="font-bold text-lg">Hello!</h3>
+          <h3 className="font-bold text-lg">
+            Fill in the details to add a donation
+          </h3>
           <p className="py-4">Click the button below to close</p>
           <div className="modal-action absolute -top-3 right-4">
             <form method="dialog">
