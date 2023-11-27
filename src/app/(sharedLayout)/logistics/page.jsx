02eeffc2091710f54614ea08229afde501ad25e7
mendsalbert <mendsalbert@gmail.com>
2023-11-27 08:31:13 +0000

    cap

--- src/app/(sharedLayout)/logistics/page.jsx
+++ src/app/(sharedLayout)/logistics/page.jsx
@@ -32,22 +32,14 @@ function LogisticsComponent() {
               placeholder="Name"
               className="input input-bordered input-md w-full dark:bg-darkblack-500"
             />
-            <select className="select select-bordered w-full  dark:bg-darkblack-500">
-              <option disabled selected>
-                War
-              </option>
-              <option>Food</option>
-              <option>Normal Orange</option>
-              <option>Normal Tomato</option>
-            </select>
-          </div>
-          {/* target amount - regioin using google map search */}
-          <div className="space-x-4 py-2 flex flex-row">
             <input
               type="number"
               placeholder="enter quantity"
               className="input input-bordered input-md w-full  dark:bg-darkblack-500"
             />
+          </div>
+          {/* target amount - regioin using google map search */}
+          <div className="space-x-4 py-2 flex flex-row">
             <select className="select select-bordered w-full  dark:bg-darkblack-500">
               <option disabled selected>
                 Isreal
