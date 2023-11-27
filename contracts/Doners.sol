173753de566d809e87299a47e4c4e451af8228bc
mendsalbert <mendsalbert@gmail.com>
2023-11-27 08:26:14 +0000

    cap

--- contracts/Doners.sol
+++ contracts/Doners.sol
@@ -1,67 +1,107 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract WarZoneDonation {
     struct Campaign {
         string name;
-        string category;
-        uint256 targetAmount;
-        uint256 amountReceived;
         string country;
-        string imageUrl;
         string description;
-        address payable targetAddress;
-        bool isActive;
+        string imageCID;
+        uint256 targetAmount;
+        uint256 totalDonationsReceived;
+        bool exists;
+        address[] donors;
+        mapping(address => uint256) donations;
     }
 
-    struct Donor {
-        address donorAddress;
-        uint256 amount;
+    struct CampaignDetails {
+        string name;
+        string country;
+        string description;
+        string imageCID;
+        uint256 targetAmount;
+        uint256 totalDonationsReceived;
     }
 
-    Campaign[] public campaigns;
-    mapping(uint256 => Donor[]) public donorsByCampaign;
+    uint256 public totalCampaignsCreated;
+    mapping(uint256 => Campaign) public campaigns;
+    mapping(bytes32 => uint256) public campaignIdByName;
 
-    function createCampaign(
-        string memory _name,
-        string memory _category,
-        uint256 _targetAmount,
-        string memory _country,
-        string memory _imageUrl,
-        string memory _description,
-        address payable _targetAddress
-    ) public {
-        campaigns.push(Campaign({
-            name: _name,
-            category: _category,
-            targetAmount: _targetAmount,
-            amountReceived: 0,
-            country: _country,
-            imageUrl: _imageUrl,
-            description: _description,
-            targetAddress: _targetAddress,
-            isActive: true
-        }));
+    event CampaignCreated(uint256 indexed campaignId, string name, uint256 targetAmount);
+    event DonationReceived(uint256 indexed campaignId, address indexed donor, uint256 amount);
+
+    function createCampaign(string memory _name, string memory _country, string memory _description, string memory _imageCID, uint256 _targetAmount) public {
+        require(_targetAmount > 0, "Target amount should be greater than zero.");
+        bytes32 nameHash = keccak256(abi.encodePacked(_name));
+        require(campaignIdByName[nameHash] == 0, "Campaign with the same name already exists.");
+
+        uint256 campaignId = totalCampaignsCreated;
+        campaignIdByName[nameHash] = campaignId;
+        totalCampaignsCreated++;
+
+        Campaign storage newCampaign = campaigns[campaignId];
+        newCampaign.name = _name;
+        newCampaign.country = _country;
+        newCampaign.description = _description;
+        newCampaign.imageCID = _imageCID;
+        newCampaign.targetAmount = _targetAmount;
+        newCampaign.exists = true;
+
+        emit CampaignCreated(campaignId, _name, _targetAmount);
     }
 
-    function donate(uint256 campaignIndex) public payable {
-        require(msg.value > 0, "Donation must be greater than 0");
-        require(campaignIndex < campaigns.length, "Campaign does not exist");
-        require(campaigns[campaignIndex].isActive, "Campaign is not active");
+    function donate(uint256 _campaignId) public payable {
+        Campaign storage selectedCampaign = campaigns[_campaignId];
+        require(selectedCampaign.exists, "Campaign does not exist.");
+        require(msg.value > 0, "Donation amount should be greater than zero.");
 
-        Campaign storage campaign = campaigns[campaignIndex];
-        campaign.amountReceived += msg.value;
-        donorsByCampaign[campaignIndex].push(Donor({ donorAddress: msg.sender, amount: msg.value }));
+        selectedCampaign.totalDonationsReceived += msg.value;
 
-        if (campaign.amountReceived >= campaign.targetAmount) {
-            campaign.targetAddress.transfer(campaign.amountReceived);
-            campaign.isActive = false;
-            // Optionally reset campaign or handle completion
+        if (selectedCampaign.donations[msg.sender] == 0) {
+            selectedCampaign.donors.push(msg.sender);
         }
+
+        selectedCampaign.donations[msg.sender] += msg.value;
+
+        emit DonationReceived(_campaignId, msg.sender, msg.value);
     }
 
-    function listAllCampaigns() public view returns (Campaign[] memory) {
-        return campaigns;
+    function getCampaignById(uint256 _campaignId) public view returns (CampaignDetails memory) {
+        Campaign storage selectedCampaign = campaigns[_campaignId];
+        require(selectedCampaign.exists, "Campaign does not exist.");
+
+        return CampaignDetails({
+            name: selectedCampaign.name,
+            country: selectedCampaign.country,
+            description: selectedCampaign.description,
+            imageCID: selectedCampaign.imageCID,
+            targetAmount: selectedCampaign.targetAmount,
+            totalDonationsReceived: selectedCampaign.totalDonationsReceived
+        });
     }
 
-    // Additional functions for viewing donations, handling campaign updates, etc.
+    function getAllCampaigns() public view returns (CampaignDetails[] memory) {
+        CampaignDetails[] memory allCampaigns = new CampaignDetails[](totalCampaignsCreated);
+        for (uint i = 0; i < totalCampaignsCreated; i++) {
+            if (campaigns[i].exists) {
+                Campaign storage campaign = campaigns[i];
+                allCampaigns[i] = CampaignDetails({
+                    name: campaign.name,
+                    country: campaign.country,
+                    description: campaign.description,
+                    imageCID: campaign.imageCID,
+                    targetAmount: campaign.targetAmount,
+                    totalDonationsReceived: campaign.totalDonationsReceived
+                });
+            }
+        }
+        return allCampaigns;
+    }
+
+    function getCampaignIdByName(string memory _name) public view returns (uint256) {
+        bytes32 nameHash = keccak256(abi.encodePacked(_name));
+        uint256 campaignId = campaignIdByName[nameHash];
+        require(campaignId != 0, "Campaign does not exist.");
+        return campaignId;
+    }
 }
