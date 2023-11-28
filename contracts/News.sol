f14371a1e4410d82aac342ad7bc6fa7b435f7e46
mendsalbert <mendsalbert@gmail.com>
2023-11-28 08:41:39 +0000

    cap

--- contracts/News.sol
+++ contracts/News.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
-import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
+import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBase.sol";
 
 contract WarNewsFeed is ChainlinkClient, VRFConsumerBase {
     using Chainlink for Chainlink.Request;
