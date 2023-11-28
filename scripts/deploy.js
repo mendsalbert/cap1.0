b2d7a50f9f535da9701c90f507b50d94cdfed368
mendsalbert <mendsalbert@gmail.com>
2023-11-28 08:52:35 +0000

    cap

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,32 +1,52 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  // Compile all contracts
+  await hre.run("compile");
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
-
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+  // Deploy the CarboonFootPrint contract
+  const CarboonFootPrint = await hre.ethers.getContractFactory(
+    "WarZoneCarbonFootPrint"
   );
+  const carboonFootPrint = await CarboonFootPrint.deploy();
+  await carboonFootPrint.deployed();
+  console.log(`CarboonFootPrint deployed to: ${carboonFootPrint.address}`);
+
+  // Deploy the Donation contract
+  // Replace these parameters with your actual parameters
+  const priceFeedAddress = "0x..."; // Chainlink Price Feed Oracle address
+  const interval = 24 * 60 * 60; // 24 hours in seconds
+  const recipientAddress = "0x..."; // Recipient address
+  const Donation = await hre.ethers.getContractFactory("WarZoneDonation");
+  const donation = await Donation.deploy(
+    priceFeedAddress,
+    interval,
+    recipientAddress
+  );
+  await donation.deployed();
+  console.log(`Donation deployed to: ${donation.address}`);
+
+  // Deploy the News contract
+  const News = await hre.ethers.getContractFactory("WarNewsFeed");
+  const news = await News.deploy();
+  await news.deployed();
+  console.log(`News deployed to: ${news.address}`);
+
+  // Deploy the Product contract
+  const Product = await hre.ethers.getContractFactory("ProductCatalog");
+  const product = await Product.deploy();
+  await product.deployed();
+  console.log(`Product deployed to: ${product.address}`);
+
+  // Deploy the Request contract
+  const Request = await hre.ethers.getContractFactory(
+    "WarZoneAssistanceRequest"
+  );
+  const request = await Request.deploy();
+  await request.deployed();
+  console.log(`Request deployed to: ${request.address}`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
