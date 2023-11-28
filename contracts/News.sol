7189a0b0aab8393589ef05a5e4fe1afce2acf578
mendsalbert <mendsalbert@gmail.com>
2023-11-28 08:40:51 +0000

    cap

--- contracts/News.sol
+++ contracts/News.sol
@@ -2,56 +2,68 @@
 pragma solidity ^0.8.0;
 
 import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 
-contract WarNewsFeed is ChainlinkClient {
+contract WarNewsFeed is ChainlinkClient, VRFConsumerBase {
     using Chainlink for Chainlink.Request;
 
-    // Chainlink parameters
+    // Chainlink Oracle parameters
     address private oracle;
     bytes32 private jobId;
     uint256 private fee;
 
+    // Chainlink VRF parameters
+    bytes32 private vrfKeyHash;
+    uint256 private vrfFee;
+    uint256 public randomResult;
+
     // Store the response data
     string public warNews;
 
-    // Event triggered when data is received
+    // Events
     event WarNewsUpdated(string news);
+    event RandomNumberRequested(bytes32 requestId);
+    event RandomNumberReceived(uint256 randomNumber);
 
-    /**
-     * @notice Constructor to set up Chainlink oracle and job details.
-     * @param _oracle The address of the Chainlink oracle.
-     * @param _jobId The job ID for the oracle request.
-     * @param _fee The LINK fee required for the oracle request.
-     */
-    constructor(address _oracle, bytes32 _jobId, uint256 _fee) {
+    constructor(
+        address _oracle,
+        bytes32 _jobId,
+        uint256 _oracleFee,
+        address _vrfCoordinator,
+        address _linkToken,
+        bytes32 _vrfKeyHash,
+        uint256 _vrfFee
+    ) 
+        VRFConsumerBase(_vrfCoordinator, _linkToken)
+    {
         setPublicChainlinkToken();
         oracle = _oracle;
         jobId = _jobId;
-        fee = _fee;
+        fee = _oracleFee;
+        vrfKeyHash = _vrfKeyHash;
+        vrfFee = _vrfFee;
     }
 
-    /**
-     * @notice Requests war news data from an external API via Chainlink oracle.
-     * @return requestId The request ID of the Chainlink request.
-     */
     function requestWarNewsData() public returns (bytes32 requestId) {
         Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
-
-        // Set the URL to perform the GET request on
         request.add("get", "https://api.news-service.com/war-news");
-
-        // Sends the request
         return sendChainlinkRequestTo(oracle, request, fee);
     }
 
-    /**
-     * @notice Callback function for Chainlink oracle, receives the news data.
-     * @param _requestId The request ID of the Chainlink request.
-     * @param _warNews The news data received from the oracle.
-     */
     function fulfill(bytes32 _requestId, string memory _warNews) public recordChainlinkFulfillment(_requestId) {
         warNews = _warNews;
         emit WarNewsUpdated(warNews);
+    }
+
+    function requestRandomNumber() public returns (bytes32 requestId) {
+        require(LINK.balanceOf(address(this)) >= vrfFee, "Not enough LINK to pay fee");
+        requestId = requestRandomness(vrfKeyHash, vrfFee);
+        emit RandomNumberRequested(requestId);
+    }
+
+    function fulfillRandomness(bytes32 _requestId, uint256 _randomness) internal override {
+        randomResult = _randomness;
+        emit RandomNumberReceived(_randomness);
     }
 
     // Additional functions as needed
