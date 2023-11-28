88cbb09888f438fccac24265adead392a0dd7a00
mendsalbert <mendsalbert@gmail.com>
2023-11-28 11:41:48 +0000

    cap

--- contracts/News.sol
+++ contracts/News.sol
@@ -2,9 +2,8 @@
 pragma solidity ^0.8.0;
 
 import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
-import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBase.sol";
 
-contract WarNewsFeed is ChainlinkClient, VRFConsumerBase {
+contract WarNewsFeed is ChainlinkClient {
     using Chainlink for Chainlink.Request;
 
     // Chainlink Oracle parameters
@@ -12,36 +11,21 @@ contract WarNewsFeed is ChainlinkClient, VRFConsumerBase {
     bytes32 private jobId;
     uint256 private fee;
 
-    // Chainlink VRF parameters
-    bytes32 private vrfKeyHash;
-    uint256 private vrfFee;
-    uint256 public randomResult;
-
     // Store the response data
     string public warNews;
 
     // Events
     event WarNewsUpdated(string news);
-    event RandomNumberRequested(bytes32 requestId);
-    event RandomNumberReceived(uint256 randomNumber);
 
     constructor(
         address _oracle,
         bytes32 _jobId,
-        uint256 _oracleFee,
-        address _vrfCoordinator,
-        address _linkToken,
-        bytes32 _vrfKeyHash,
-        uint256 _vrfFee
-    ) 
-        VRFConsumerBase(_vrfCoordinator, _linkToken)
-    {
+        uint256 _oracleFee
+    ) {
         setPublicChainlinkToken();
         oracle = _oracle;
         jobId = _jobId;
         fee = _oracleFee;
-        vrfKeyHash = _vrfKeyHash;
-        vrfFee = _vrfFee;
     }
 
     function requestWarNewsData() public returns (bytes32 requestId) {
@@ -53,17 +37,6 @@ contract WarNewsFeed is ChainlinkClient, VRFConsumerBase {
     function fulfill(bytes32 _requestId, string memory _warNews) public recordChainlinkFulfillment(_requestId) {
         warNews = _warNews;
         emit WarNewsUpdated(warNews);
-    }
-
-    function requestRandomNumber() public returns (bytes32 requestId) {
-        require(LINK.balanceOf(address(this)) >= vrfFee, "Not enough LINK to pay fee");
-        requestId = requestRandomness(vrfKeyHash, vrfFee);
-        emit RandomNumberRequested(requestId);
-    }
-
-    function fulfillRandomness(bytes32 _requestId, uint256 _randomness) internal override {
-        randomResult = _randomness;
-        emit RandomNumberReceived(_randomness);
     }
 
     // Additional functions as needed
