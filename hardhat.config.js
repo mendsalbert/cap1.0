406743fa248d70a41ad2fd5f2d5cfab214e19bce
mendsalbert <mendsalbert@gmail.com>
2023-11-28 18:11:45 +0000

    cap

--- hardhat.config.js
+++ hardhat.config.js
@@ -24,13 +24,6 @@ module.exports = {
       url: "https://polygon-mumbai.g.alchemy.com/v2/2bGIFu-iEnl9RvAOTe1ddZI2gBnuYQGS",
       accounts: [privateKey],
     },
-    kovan: {
-      url: "https://kovan.infura.io/v3/745fcbe1f649402c9063fa946fdbb84c",
-      accounts: [privateKey],
-      gas: 2100000,
-      gasPrice: 8000000000,
-      // network_id: 42,
-    },
   },
   solidity: {
     version: "0.8.4",
