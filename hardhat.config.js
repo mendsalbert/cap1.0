cbe1ebbde89e3aa495ff3733a3b29a2827f6b2ec
mendsalbert <mendsalbert@gmail.com>
2023-11-28 09:07:13 +0000

    cap

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,44 @@
-require("@nomicfoundation/hardhat-toolbox");
+/* hardhat.config.js */
+require("@nomiclabs/hardhat-waffle");
+const fs = require("fs");
+const privateKey = fs.readFileSync("secret.txt").toString();
+const projectId = "8b7ba5517c414450a93ec7334975a7fe";
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    //  unused configuration commented out for now
+    mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      // url: "https://goerli.infura.io/v3/8b7ba5517c414450a93ec7334975a7fe",
+      accounts: [privateKey],
+    },
+    mainet: {
+      url: `https://palm-mainnet.infura.io/v3/${projectId}`,
+      // accounts: [privateKey],
+    },
+    matic: {
+      url: "https://polygon-mumbai.g.alchemy.com/v2/2bGIFu-iEnl9RvAOTe1ddZI2gBnuYQGS",
+      accounts: [privateKey],
+    },
+    kovan: {
+      url: "https://kovan.infura.io/v3/745fcbe1f649402c9063fa946fdbb84c",
+      accounts: [privateKey],
+      gas: 2100000,
+      gasPrice: 8000000000,
+      // network_id: 42,
+    },
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
 };
