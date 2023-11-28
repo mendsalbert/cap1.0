3d9e4afb5c2bb56b874bc8ea49362bf271b41db0
mendsalbert <mendsalbert@gmail.com>
2023-11-28 18:42:52 +0000

    cap

--- hardhat.config.js
+++ hardhat.config.js
@@ -16,14 +16,6 @@ module.exports = {
       // url: "https://goerli.infura.io/v3/8b7ba5517c414450a93ec7334975a7fe",
       accounts: [privateKey],
     },
-    mainet: {
-      url: `https://palm-mainnet.infura.io/v3/${projectId}`,
-      // accounts: [privateKey],
-    },
-    matic: {
-      url: "https://polygon-mumbai.g.alchemy.com/v2/2bGIFu-iEnl9RvAOTe1ddZI2gBnuYQGS",
-      accounts: [privateKey],
-    },
   },
   solidity: {
     version: "0.8.4",
