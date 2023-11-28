e92f4bcb6ace34844ed18e0274e0770fa7a5c681
mendsalbert <mendsalbert@gmail.com>
2023-11-28 18:40:27 +0000

    cap

--- contracts/Donation.sol
+++ contracts/Donation.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "@chainlink/contracts/src/v0.8/automation/KeeperCompatible.sol";
 
-contract WarZoneDonation {
+contract Donation {
 
     // Chainlink Price Feed
     AggregatorV3Interface internal priceFeed;
