2a8e97688ff998394e2164f27e367fac55616a73
mendsalbert <mendsalbert@gmail.com>
2023-11-28 18:40:21 +0000

    cap

--- contracts/CarbonFootPrint.sol
+++ contracts/CarbonFootPrint.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract WarZoneCarbonFootPrint {
+contract CarbonFootPrint {
     struct Project {
         string name;
         string country;
