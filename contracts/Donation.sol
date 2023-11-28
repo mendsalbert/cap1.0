3ee18126af8f80f4e07b5a2fbedc6b5314374153
mendsalbert <mendsalbert@gmail.com>
2023-11-28 07:39:44 +0000

    cap

--- contracts/Donation.sol
+++ contracts/Donation.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-import "@chainlink/contracts/src/v0.8/KeeperCompatible.sol";
+import "@chainlink/contracts/src/v0.8/automation/KeeperCompatible.sol";
 
 contract WarZoneDonation {
 
