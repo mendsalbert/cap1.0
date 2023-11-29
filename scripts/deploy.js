fb072e567324a42a7be01b5afb2e04769b8e95c2
mendsalbert <mendsalbert@gmail.com>
2023-11-29 10:40:16 +0000

    cap

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,76 +2,59 @@ const hre = require("hardhat");
 const fs = require("fs");
 const path = require("path");
 
-async function main() {
-  const [deployer] = await hre.ethers.getSigners();
+async function deployContract(name, constructorArgs = []) {
+  const ContractFactory = await hre.ethers.getContractFactory(name);
+  const contract = await ContractFactory.deploy(...constructorArgs);
+  await contract.deployed();
 
-  console.log("Deploying contracts with the account:", deployer.address);
+  console.log(`${name} contract address:`, contract.address);
 
-  // Parameters for the Donation contract constructor
-  const priceFeedAddress = "0x9326bfa02add2366b30bacb125260af641031331"; // The address of the Chainlink Price Feed contract
-  const interval = 86400; // 24 hours in seconds
-  const recipientAddress = "0x5b29f4a7c1715ef2f974f21216fffb1834a33f04"; // The recipient address
-
-  // Deploy CarbonFootPrint
-  const CarbonFootPrint = await hre.ethers.getContractFactory(
-    "CarbonFootPrint"
-  );
-  const carbonFootPrint = await CarbonFootPrint.deploy();
-  await carbonFootPrint.deployed();
-  console.log("CarbonFootPrint contract address:", carbonFootPrint.address);
-
-  // Copy the ABI file to the desired location
+  // Copy ABI file
   const abiSourcePath = path.join(
     __dirname,
-    "../artifacts/contracts/CarbonFootPrint.sol/CarbonFootPrint.json"
+    `../artifacts/contracts/${name}.sol/${name}.json`
   );
   const abiDestinationPath = path.join(
     __dirname,
-    "../utils/CarbonFootPrintC/CarbonFootPrint.json"
+    `../utils/${name}C/${name}.json`
   );
   fs.copyFileSync(abiSourcePath, abiDestinationPath);
-  console.log("ABI file copied!");
+  console.log(`${name} ABI file copied!`);
 
-  // Update the contract address in contract.js
-  const contractJsPath = path.join(
-    __dirname,
-    "../utils/CarbonFootPrintC/contract.js"
-  );
+  // Update contract address
+  const contractJsPath = path.join(__dirname, `../utils/${name}C/contract.js`);
   let contractJsContent = fs.readFileSync(contractJsPath, { encoding: "utf8" });
+  const regex = new RegExp(`(const contractAddress = ")(.*)(")`);
   contractJsContent = contractJsContent.replace(
-    /(const contractAddress = ")(.*)(")/,
-    `$1${carbonFootPrint.address}$3`
+    regex,
+    `$1${contract.address}$3`
   );
   fs.writeFileSync(contractJsPath, contractJsContent);
-  console.log("Contract address updated in contract.js!");
+  console.log(`${name} contract address updated in contract.js!`);
 
-  // Deploy Donation
-  const Donation = await hre.ethers.getContractFactory("Donation");
-  const donation = await Donation.deploy(
+  return contract;
+}
+
+async function main() {
+  const [deployer] = await hre.ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  // Parameters for the Donation contract constructor
+  const priceFeedAddress = "0x9326bfa02add2366b30bacb125260af641031331";
+  const interval = 86400; // 24 hours in seconds
+  const recipientAddress = "0x5b29f4a7c1715ef2f974f21216fffb1834a33f04";
+
+  // Deploy contracts
+  await deployContract("CarbonFootPrint");
+  await deployContract("Donation", [
     priceFeedAddress,
     interval,
-    recipientAddress
-  );
-  await donation.deployed();
-  console.log("Donation contract address:", donation.address);
-
-  // // Deploy News
-  // const News = await hre.ethers.getContractFactory("News");
-  // const news = await News.deploy();
-  // await news.deployed();
-  // console.log("News contract address:", news.address);
-
-  // Deploy Product
-  const Product = await hre.ethers.getContractFactory("Product");
-  const product = await Product.deploy();
-  await product.deployed();
-  console.log("Product contract address:", product.address);
-
-  // Deploy Request
-  const Request = await hre.ethers.getContractFactory("RequestC");
-  const request = await Request.deploy();
-  await request.deployed();
-  console.log("Request contract address:", request.address);
+    recipientAddress,
+  ]);
+  // Uncomment if News contract is ready to be deployed
+  // await deployContract("News");
+  await deployContract("Product");
+  await deployContract("RequestC"); // Assuming the contract name is RequestC
 }
 
 main()
