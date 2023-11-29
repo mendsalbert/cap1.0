0d36df2e8f59b565969ec7a45834da7007c16c81
mendsalbert <mendsalbert@gmail.com>
2023-11-29 10:38:36 +0000

    cap

---
+++ utils/RequestC/contract.js
@@ -0,0 +1,17 @@
+import { ethers } from "ethers";
+import Donation from "./Donation.json";
+
+export const DonationContract = async () => {
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const { ethereum } = window;
+  const contractAddress = "";
+  if (ethereum) {
+    const signer = provider.getSigner();
+    const contractReader = new ethers.Contract(
+      contractAddress,
+      Donation.abi,
+      signer
+    );
+    return contractReader;
+  }
+};
