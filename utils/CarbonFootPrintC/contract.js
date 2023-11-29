ddee174f76aa827ce3c6a0e6632a7955d4ce9dfa
mendsalbert <mendsalbert@gmail.com>
2023-11-29 10:32:19 +0000

    cap

--- utils/CarbonFootPrintC/contract.js
+++ utils/CarbonFootPrintC/contract.js
@@ -4,7 +4,7 @@ import CarboonFootprint from "./CarbonFootPrint.json";
 export const CarboonFootPrintContract = async () => {
   const provider = new ethers.providers.Web3Provider(window.ethereum);
   const { ethereum } = window;
-  const contractAddress = "";
+  const contractAddress = "0x1316456Af510c9DECe0ca0BbFFeedaC2C1AE4a94";
   if (ethereum) {
     const signer = provider.getSigner();
     const contractReader = new ethers.Contract(
