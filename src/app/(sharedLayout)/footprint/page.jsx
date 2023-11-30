f8917de6ac0dee9280d458cfdf7e592e9fd53f60
mendsalbert <mendsalbert@gmail.com>
2023-11-30 12:18:48 +0000

    cap

--- src/app/(sharedLayout)/footprint/page.jsx
+++ src/app/(sharedLayout)/footprint/page.jsx
@@ -72,7 +72,7 @@ function FootPrintComponent() {
               placeholder="eg 5TH"
               className="input input-bordered input-md w-full  dark:bg-darkblack-500"
             />
-            <div className="w-full">
+            <div className="">
               <GooglePlacesAutocomplete
                 apiKey="AIzaSyBfkyKitodTym6Q7oMEWgHAEDP9FuLrP8k"
                 selectProps={{
