8daa7c5e900cfae3caa1a5a2667498d06ee78765
mendsalbert <mendsalbert@gmail.com>
2023-11-30 11:36:38 +0000

    cap

--- src/app/(sharedLayout)/footprint/page.jsx
+++ src/app/(sharedLayout)/footprint/page.jsx
@@ -22,7 +22,7 @@ function FootPrintComponent() {
   const [amount, setamount] = useState("");
   const [deadline, setdeadline] = useState("");
   const [txPending, setTxPending] = useState(false);
-  console.log("location", location);
+  console.log("location", value);
   const handleSelect = (location) => {
     console.log(location);
     setSelectedLocation(location);
