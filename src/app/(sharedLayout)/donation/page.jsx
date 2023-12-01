9f276744729133a38bacef07e213671358c89270
mendsalbert <mendsalbert@gmail.com>
2023-12-01 14:48:01 +0000

    cap

--- src/app/(sharedLayout)/donation/page.jsx
+++ src/app/(sharedLayout)/donation/page.jsx
@@ -60,14 +60,6 @@ function MyWallet() {
               placeholder="eg 5TH"
               className="input input-bordered input-md w-full  dark:bg-darkblack-500"
             />
-            <select className="select select-bordered w-full  dark:bg-darkblack-500">
-              <option disabled selected>
-                Isreal
-              </option>
-              <option>Gaza</option>
-              <option>Palenstine</option>
-              <option>Normal Tomato</option>
-            </select>
           </div>
           {/* file input */}
           <div className="form-control w-full py-2">
