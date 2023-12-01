e48204e82f62c71f400d6c08a0efdc1f841cae6a
mendsalbert <mendsalbert@gmail.com>
2023-12-01 12:09:51 +0000

    cap

--- src/component/footPrint/FootPrint.jsx
+++ src/component/footPrint/FootPrint.jsx
@@ -63,7 +63,22 @@ function FootPrint({ footprints }) {
     console.log(value);
     setTxPending(false);
   }
-  // const { img, title, category, text, status } = footprint;
+
+  //pending transaction modal
+
+  <dialog id="my_modal_9" className="modal">
+    <div className="modal-box">
+      <h3 className="font-bold text-lg">Hello!</h3>
+      <p className="py-4">Press ESC key or click the button below to close</p>
+      <div className="modal-action">
+        <form method="dialog">
+          {/* if there is a button in form, it will close the modal */}
+          <button className="btn">Close</button>
+        </form>
+      </div>
+    </div>
+  </dialog>;
+
   return (
     <>
       {footprints?.map((footprint, i) => {
