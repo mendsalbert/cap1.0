ca7fe0bacf6b9d7f3f13c29b5102649e0f4cbd9a
mendsalbert <mendsalbert@gmail.com>
2023-12-01 11:53:10 +0000

    cap

--- src/component/footPrint/FootPrint.jsx
+++ src/component/footPrint/FootPrint.jsx
@@ -53,7 +53,7 @@ function FootPrint({ footprints }) {
     console.log("name", name);
     console.log("donationRecienved", totalDonationsReceived?.toString());
     console.log("carboonremoved", carbonOffsetRemoved?.toString());
-    console.log(id);
+    console.log("id", id);
   };
   const dateString = new Date(deadline?.toString() * 1000).toLocaleDateString(
     "en-GB"
