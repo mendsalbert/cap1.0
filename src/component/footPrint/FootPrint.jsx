0e5cbc59c9884df4ba2550911bb7f9c14d2f72b2
mendsalbert <mendsalbert@gmail.com>
2023-12-01 10:02:41 +0000

    cap

--- src/component/footPrint/FootPrint.jsx
+++ src/component/footPrint/FootPrint.jsx
@@ -134,8 +134,8 @@ function FootPrint({ footprints }) {
                   <span>
                     <IconClock />
                   </span>
-                  <p>{dateString}</p>
-                  {/* <p>{footprint?.deadline?.toString()}</p> */}
+                  {/* <p>{dateString}</p> */}
+                  <p>{deadline?.toString()}</p>
                 </span>
                 <span className="flex flex-col items-center ">
                   <span>
