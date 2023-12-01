6f1beb734f6974a1eb6db665afa4ddf151a434d1
mendsalbert <mendsalbert@gmail.com>
2023-12-01 14:20:14 +0000

    cap

--- src/app/(sharedLayout)/footprint/page.jsx
+++ src/app/(sharedLayout)/footprint/page.jsx
@@ -182,7 +182,6 @@ function FootPrintComponent() {
           </p>
           <div className="modal-action">
             <form method="dialog">
-              {/* if there is a button in form, it will close the modal */}
               <button className="btn">Close</button>
             </form>
           </div>
