cdc48b234c55cc3ffa30d7b0f18e803bc7a75114
mendsalbert <mendsalbert@gmail.com>
2023-12-01 12:12:55 +0000

    cap

--- src/component/footPrint/FootPrint.jsx
+++ src/component/footPrint/FootPrint.jsx
@@ -59,6 +59,7 @@ function FootPrint({ footprints }) {
 
   async function onaddDonation() {
     setTxPending(true);
+    document.getElementById("my_modal_9").showModal();
     let value = await donateToCarbonFootPrintProject(id, amount);
     console.log(value);
     setTxPending(false);
@@ -235,7 +236,6 @@ function FootPrint({ footprints }) {
               />
               <button
                 onClick={() => {
-                  document.getElementById("my_modal_1").showModal();
                   onaddDonation();
                 }}
                 className="bg-[#21c55d] mt-4 hover:bg-green-600 text-white rounded-full px-10  py-2 text-lg"
