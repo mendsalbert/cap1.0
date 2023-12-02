1b23d636bdcf4faa9ac0f90b37337daca8215cac
mendsalbert <mendsalbert@gmail.com>
2023-12-02 21:20:29 +0000

    cap

--- src/app/(sharedLayout)/task/page.jsx
+++ src/app/(sharedLayout)/task/page.jsx
@@ -63,14 +63,6 @@ function Task() {
               <tr>
                 <td>
                   <div className="flex items-center gap-3">
-                    <div className="avatar">
-                      <div className="mask mask-squircle w-12 h-12">
-                        <img
-                          src="/tailwind-css-component-profile-2@56w.png"
-                          alt="Avatar Tailwind CSS Component"
-                        />
-                      </div>
-                    </div>
                     <div>
                       <div className="font-bold">Hart Hagerty</div>
                       <div className="text-sm opacity-50">United States</div>
