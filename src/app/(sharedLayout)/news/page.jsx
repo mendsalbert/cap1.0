9c5d1467683d310f7ae8f1b3ecb1c1b80afe4ff0
mendsalbert <mendsalbert@gmail.com>
2023-12-02 18:34:13 +0000

    cap

--- src/app/(sharedLayout)/news/page.jsx
+++ src/app/(sharedLayout)/news/page.jsx
@@ -99,7 +99,7 @@ function News() {
                   <td>
                     <div className="flex items-center gap-3">
                       <div className="avatar">
-                        <div className="mask mask-squircle w-20 h-20">
+                        <div className="mask mask-squircle w-24 h-24">
                           <img
                             src={news?.urlToImage}
                             alt="Avatar Tailwind CSS Component"
