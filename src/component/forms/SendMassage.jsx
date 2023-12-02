572dc2fcbabb4450ab1a5ddfbcd91680061d8c7f
mendsalbert <mendsalbert@gmail.com>
2023-12-02 12:51:07 +0000

    cap

--- src/component/forms/SendMassage.jsx
+++ src/component/forms/SendMassage.jsx
@@ -66,7 +66,7 @@ function SendMassage() {
           .map((message, index) => (
             <>
               {message?.sender != "user" ? (
-                <p className="absolute left-12 top-10">
+                <p className="absolute left-11 top-10">
                   <Image
                     priority={true}
                     height={40}
