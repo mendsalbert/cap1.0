21c6c4973bab6526024f343657bbdc909ab0b330
mendsalbert <mendsalbert@gmail.com>
2023-12-02 17:08:39 +0000

    cap

--- src/app/(sharedLayout)/news/page.jsx
+++ src/app/(sharedLayout)/news/page.jsx
@@ -66,7 +66,7 @@ function News() {
 
   console.log("newsData", newsData);
   console.log("newsData");
-  alert("newdate");
+  alert(newsData);
   return (
     <>
       <div className="relative w-full overflow-hidden">
