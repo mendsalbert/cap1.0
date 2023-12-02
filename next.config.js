6c6883936b13ccce90b50270558932c418aa1cab
mendsalbert <mendsalbert@gmail.com>
2023-12-02 17:23:34 +0000

    cap

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["s.yimg.com"], // Add the domain(s) you want to allow images from
+  },
+};
 
 module.exports = nextConfig;
