9636b32df375156241b0f186a7b18f029cd6df7e
mendsalbert <mendsalbert@gmail.com>
2023-12-02 22:05:29 +0000

    cap

--- src/app/(sharedLayout)/task/page.jsx
+++ src/app/(sharedLayout)/task/page.jsx
@@ -29,32 +29,30 @@ function Task() {
               <tr></tr>
             </thead>
             <tbody>
-{request?.map((request, item)=>(
-  <tr>
-  <td>
-    <div className="flex items-center gap-3">
-      <div>
-        <div className="font-bold">Hart Hagerty</div>
-        <div className="text-sm opacity-50">United States</div>
-      </div>
-    </div>
-  </td>
-  <td>
-    Zemlak, Daniel and Leannon
-    <br />
-    <span className="badge badge-ghost badge-sm">
-      Desktop Support Technician
-    </span>
-  </td>
-  <td>Purple</td>
-  <th>
-    <button className="btn btn-ghost btn-xs">details</button>
-  </th>
-</tr>
-))}
-            
+              {request?.map((request, item) => (
+                <tr>
+                  <td>
+                    <div className="flex items-center gap-3">
+                      <div>
+                        <div className="font-bold">Hart Hagerty</div>
+                        <div className="text-sm opacity-50">United States</div>
+                      </div>
+                    </div>
+                  </td>
+                  <td>
+                    Zemlak, Daniel and Leannon
+                    <br />
+                    <span className="badge badge-ghost badge-sm">
+                      Desktop Support Technician
+                    </span>
+                  </td>
+                  <td>Purple</td>
+                  <th>
+                    <button className="btn btn-ghost btn-xs">details</button>
+                  </th>
+                </tr>
+              ))}
             </tbody>
-      
           </table>
         </div>
       </section>
