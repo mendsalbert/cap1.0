d226e612dba34e8bb7f04682fccd04d4c7c5b21f
mendsalbert <mendsalbert@gmail.com>
2023-12-02 10:08:04 +0000

    cap

--- contracts/Product.sol
+++ contracts/Product.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-
+ 
 contract Product {
     struct Product {
         string name;
@@ -8,23 +8,25 @@ contract Product {
         uint256 dateAdded;
         string description;
         string imageUrl;
-        string country; // New field for the country
         bool exists;
+        string supplyLocation;
     }
 
     uint256 public totalProducts;
     mapping(uint256 => Product) public products;
     mapping(bytes32 => uint256) public productIdByName;
 
-    event ProductAdded(uint256 indexed productId, string name, uint256 quantity, string country);
+    event ProductSupplied(uint256 indexed productId, uint256 quantity, string supplyLocation); // New event for supply
+    event ProductAdded(uint256 indexed productId, string name, uint256 quantity);
 
-    function addProduct(string memory _name, uint256 _quantity, string memory _description, string memory _imageUrl, string memory _country) public {
+    function addProduct(string memory _name, uint256 _quantity, string memory _description, string memory _imageUrl) public {
         require(_quantity > 0, "Quantity should be greater than zero.");
         bytes32 nameHash = keccak256(abi.encodePacked(_name));
         require(productIdByName[nameHash] == 0, "Product with the same name already exists.");
 
-        uint256 productId = totalProducts++;
+        uint256 productId = totalProducts;
         productIdByName[nameHash] = productId;
+        totalProducts++;
 
         Product storage newProduct = products[productId];
         newProduct.name = _name;
@@ -32,40 +34,53 @@ contract Product {
         newProduct.dateAdded = block.timestamp;
         newProduct.description = _description;
         newProduct.imageUrl = _imageUrl;
-        newProduct.country = _country; // Setting the country of the product
         newProduct.exists = true;
 
-        emit ProductAdded(productId, _name, _quantity, _country);
+        emit ProductAdded(productId, _name, _quantity);
     }
 
-    function getProductById(uint256 _productId) public view returns (string memory, uint256, uint256, string memory, string memory, string memory) {
-        require(_productId < totalProducts, "Product does not exist.");
+    function getProductById(uint256 _productId) public view returns (string memory, uint256, uint256, string memory, string memory) {
         Product storage selectedProduct = products[_productId];
         require(selectedProduct.exists, "Product does not exist.");
 
-        return (
-            selectedProduct.name, 
-            selectedProduct.quantity, 
-            selectedProduct.dateAdded, 
-            selectedProduct.description, 
-            selectedProduct.imageUrl,
-            selectedProduct.country // Returning the country as well
-        );
+        return (selectedProduct.name, selectedProduct.quantity, selectedProduct.dateAdded, selectedProduct.description, selectedProduct.imageUrl);
     }
 
     function getAllProducts() public view returns (Product[] memory) {
         Product[] memory allProducts = new Product[](totalProducts);
         for (uint i = 0; i < totalProducts; i++) {
             if (products[i].exists) {
-                allProducts[i] = products[i];
+                Product storage product = products[i];
+                allProducts[i] = Product({
+                    name: product.name,
+                    quantity: product.quantity,
+                    dateAdded: product.dateAdded,
+                    description: product.description,
+                    imageUrl: product.imageUrl,
+                    exists: product.exists,
+                    supplyLocation:product.supplyLocation
+
+                });
             }
         }
         return allProducts;
     }
 
+    function supply(uint256 _productId, uint256 _quantity, string memory _supplyLocation) public {
+        Product storage product = products[_productId];
+        require(product.exists, "Product does not exist.");
+        require(_quantity > 0 && _quantity <= product.quantity, "Insufficient quantity in stock.");
+
+        product.quantity -= _quantity; // Decrease the quantity
+        product.supplyLocation = _supplyLocation; // Update the supply location
+
+        emit ProductSupplied(_productId, _quantity, _supplyLocation);
+    }
+    
     function getProductIdByName(string memory _name) public view returns (uint256) {
         bytes32 nameHash = keccak256(abi.encodePacked(_name));
-        require(productIdByName[nameHash] != 0, "Product does not exist.");
-        return productIdByName[nameHash];
+        uint256 productId = productIdByName[nameHash];
+        require(productId != 0, "Product does not exist.");
+        return productId;
     }
 }
