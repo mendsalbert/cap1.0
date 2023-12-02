b62998f1d920afe146ad408b5c227dd29e1d5faf
mendsalbert <mendsalbert@gmail.com>
2023-12-02 15:11:53 +0000

    cap

--- src/app/(sharedLayout)/news/page.jsx
+++ src/app/(sharedLayout)/news/page.jsx
@@ -103,7 +103,7 @@ function News() {
         </div>
       </div>
       <section className="2xl:w-70 w-full 2xl:mb-0 mb-6">
-        {/* <ListTab pageSize={9} /> */}
+        <ListTab pageSize={9} />
       </section>
       {/* <section className="2xl:flex-1 w-full">
         <Wallet />
