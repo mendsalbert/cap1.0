4457c0fc6a73dd765fb1a25622ead40fbfd903d0
mendsalbert <mendsalbert@gmail.com>
2023-12-02 22:46:29 +0000

    cap

--- src/app/(sharedLayout)/task/page.jsx
+++ src/app/(sharedLayout)/task/page.jsx
@@ -51,7 +51,7 @@ function Task() {
                       </div>
                     </div>
                   </td>
-                  <td>{request.status}</td>
+                  <td>{Boolean(request.status)}</td>
                   <td>
                     {new Date(
                       request?.timestamp?.toString() * 1000
