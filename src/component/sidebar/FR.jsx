9930458a5fab2136bf5d0262f7ccb266d31d4463
mendsalbert <mendsalbert@gmail.com>
2023-12-02 22:43:05 +0000

    cap

--- src/component/sidebar/FR.jsx
+++ src/component/sidebar/FR.jsx
@@ -118,12 +118,15 @@ function FR({ handleActive }) {
                         Tasks
                         <div className="avatar online placeholder">
                           <div className="bg-red-500 text-white rounded-full w-6">
-                            <span className="text-sm">AI</span>
+                            <span className="text-sm">
+                              {" "}
+                              {requests?.length}{" "}
+                            </span>
                           </div>
                         </div>
                         {/* <span className="bg-pink-600 text-sm p-2 w-5 h-4 ml-2 text-center text-white rounded-full">
                           {" "}
-                          {requests?.length}{" "}
+                         
                         </span> */}
                       </span>
                     </div>
