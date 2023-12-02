16d135f0aad671e65fdf975d6a9675b900839449
mendsalbert <mendsalbert@gmail.com>
2023-12-02 18:10:34 +0000

    cap

--- src/app/(sharedLayout)/news/page.jsx
+++ src/app/(sharedLayout)/news/page.jsx
@@ -92,7 +92,7 @@ function News() {
                   </div>
                   <div>
                     <p className="text-2xl font-bold">{news?.title}</p>
-                    <h2 className="text-lg">{news?.title}</h2>
+                    <h2 className="text-lg">{news?.description}</h2>
                     <p className="text-sm">{news?.title}</p>
                   </div>
                 </div>
