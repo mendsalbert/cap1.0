5e9bc36218b909724a65e616c8b097a93cf620f5
mendsalbert <mendsalbert@gmail.com>
2023-12-03 09:21:39 +0000

    cap

---
+++ src/app/(sharedLayout)/ngo/page.jsx
@@ -0,0 +1,12 @@
+// pages/client.js
+import ClientComponent from "@/components/ClientComponent"; // Import your CLIENT component
+
+function ClientPage() {
+  return (
+    <div>
+      <ClientComponent />
+    </div>
+  );
+}
+
+export default ClientPage;
