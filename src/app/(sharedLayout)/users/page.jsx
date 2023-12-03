ab559803f9fa73fbd4392acd18298d73610725a4
mendsalbert <mendsalbert@gmail.com>
2023-12-03 15:54:32 +0000

    user data error

--- src/app/(sharedLayout)/users/page.jsx
+++ src/app/(sharedLayout)/users/page.jsx
@@ -1,13 +1,11 @@
-import UserFilter from "@/component/forms/UserFilter";
-import UsersList from "@/component/user/UsersList";
 import RightSidebar from "@/component/user/RightSidebar";
 
 function Users() {
   return (
     <>
       <div className="2xl:flex-1 w-full">
-        <UserFilter />
-        <UsersList />
+        {/* <UserFilter /> */}
+        {/* <UsersList /> */}
       </div>
       <RightSidebar />
     </>
